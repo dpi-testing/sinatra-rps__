5a32e89c8e942beadab1c3206333f66356fdc82e
Chandhu Bhumireddy <cbhum@uic.edu>
2025-02-06 14:04:03 -0600

    changes

--- app.rb
+++ app.rb
@@ -2,8 +2,75 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
 end
+
+get("/:move") do
+  @player_move = params.fetch("move")
+  puts @player_move
+  moves = ["rock" , "paper" , "scissor"]
+  @comp_move = moves.sample
+
+  if @player_move == @comp_move
+    @outcome = "tied"
+  elsif @player_move == "rock" 
+    if @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "win"
+    end
+
+  elsif @player_move == "paper" 
+    if @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "win"
+    end
+
+  elsif @player_move == "scissors" 
+    if @comp_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "win"
+    end
+  end
+  erb(:move)
+end
+
+  
+
+  
+
+
+# get("/paper") do
+#   moves = ["rock" , "paper" , "scissor"]
+#   @comp_move = moves.sample
+
+#   if @comp_move == "scissors"
+#     @outcome = "lost"
+#   elsif @comp_move == "paper"
+#     @outcome = "tied"
+#   else
+#     @outcome = "won"
+#   end
+
+#   erb(:paper)
+
+# end
+
+
+# get("/scissors") do
+#   moves = ["rock" , "paper" , "scissor"]
+#   @comp_move = moves.sample
+
+#   if @comp_move == "scissors"
+#     @outcome = "tied"
+#   elsif @comp_move == "rock"
+#     @outcome = "lost"
+#   else
+#     @outcome = "won"
+#   end
+
+#   erb(:scissors)
+
+# end
