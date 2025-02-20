709a90eaea2bd1adcb4c5c9854eb09ac1bdc97e9
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-02-20 19:27:52 +0000

    refactored using hash

--- app.rb
+++ app.rb
@@ -11,66 +11,19 @@ get("/:move") do
   moves = ["rock" , "paper" , "scissor"]
   @comp_move = moves.sample
 
+  winning_moves = {
+    "rock" => "scissor",
+    "paper" => "rock",
+    "scissor" => "paper"
+  }
+
   if @player_move == @comp_move
     @outcome = "tied"
-  elsif @player_move == "rock" 
-    if @comp_move == "paper"
-      @outcome = "lost"
-    else
-      @outcome = "win"
-    end
-
-  elsif @player_move == "paper" 
-    if @comp_move == "scissors"
-      @outcome = "lost"
-    else
-      @outcome = "win"
-    end
-
-  elsif @player_move == "scissors" 
-    if @comp_move == "rock"
-      @outcome = "lost"
-    else
-      @outcome = "win"
-    end
+  elsif winning_moves[@player_move] == @comp_move
+    @outcome = "win"
+  else
+    @outcome = "lost"
   end
+
   erb(:move)
 end
-
-  
-
-  
-
-
-# get("/paper") do
-#   moves = ["rock" , "paper" , "scissor"]
-#   @comp_move = moves.sample
-
-#   if @comp_move == "scissors"
-#     @outcome = "lost"
-#   elsif @comp_move == "paper"
-#     @outcome = "tied"
-#   else
-#     @outcome = "won"
-#   end
-
-#   erb(:paper)
-
-# end
-
-
-# get("/scissors") do
-#   moves = ["rock" , "paper" , "scissor"]
-#   @comp_move = moves.sample
-
-#   if @comp_move == "scissors"
-#     @outcome = "tied"
-#   elsif @comp_move == "rock"
-#     @outcome = "lost"
-#   else
-#     @outcome = "won"
-#   end
-
-#   erb(:scissors)
-
-# end
